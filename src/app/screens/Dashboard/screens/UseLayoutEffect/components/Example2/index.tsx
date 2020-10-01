8610824c2de662746be7ac835fa0b572296b0075
alejobh <alejo0395@gmail.com>
2020-10-01 13:16:46 -0500

    Added title

--- src/app/screens/Dashboard/screens/UseLayoutEffect/components/Example2/index.tsx
+++ src/app/screens/Dashboard/screens/UseLayoutEffect/components/Example2/index.tsx
@@ -13,18 +13,23 @@ function Example2() {
   const box2Ref = useRef<Nullable<HTMLButtonElement>>(null);
 
   return (
-    <div className="row center space-between">
-      <div className={`${styles.container} m-right-10`}>
-        <button type="button" ref={box1Ref} className="box" onClick={() => setShowMessage1(prev => !prev)}>
-          useEffect
-        </button>
-        {showMessage1 && <Message boxRef={box1Ref}>useEffect message</Message>}
-      </div>
-      <div className={styles.container}>
-        <button type="button" ref={box2Ref} className="box" onClick={() => setShowMessage2(prev => !prev)}>
-          useLayoutEffect
-        </button>
-        {showMessage2 && <MessageLayoutEffect boxRef={box2Ref}>useLayoutEffect message</MessageLayoutEffect>}
+    <div className="column center">
+      <h2 className="m-bottom-3">Example 2</h2>
+      <div className="row center space-between">
+        <div className={`${styles.container} m-right-10`}>
+          <button type="button" ref={box1Ref} className="box" onClick={() => setShowMessage1(prev => !prev)}>
+            useEffect
+          </button>
+          {showMessage1 && <Message boxRef={box1Ref}>useEffect message</Message>}
+        </div>
+        <div className={styles.container}>
+          <button type="button" ref={box2Ref} className="box" onClick={() => setShowMessage2(prev => !prev)}>
+            useLayoutEffect
+          </button>
+          {showMessage2 && (
+            <MessageLayoutEffect boxRef={box2Ref}>useLayoutEffect message</MessageLayoutEffect>
+          )}
+        </div>
       </div>
     </div>
   );
