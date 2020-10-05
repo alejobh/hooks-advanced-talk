fd5c8cc862ae03ac8dd411416b2c96620d74c889
Pablo Ferro <pablo.pyp@gmail.com>
2020-10-05 18:54:07 -0300

    Add UseCallbackMemo example

--- src/services/GhibliService.ts
+++ src/services/GhibliService.ts
@@ -5,8 +5,19 @@ type ApiError = {
   message: string;
 };
 
+export interface MovieRating {
+  title: string;
+  rating: number;
+}
+
+export interface UserProfile {
+  name: string;
+  movieHistory: MovieRating[];
+}
+
 const api = createApi(process.env.REACT_APP_GHIBLI_API);
 
 export default {
-  getMovies: (limit: number) => api.get<GhibliMovieI[], ApiError>('/films', { ...(limit && { limit }) })
+  getMovies: (limit: number) => api.get<GhibliMovieI[], ApiError>('/films', { ...(limit && { limit }) }),
+  saveProfile: (user: UserProfile) => Promise.resolve({ ok: true, data: user })
 };
